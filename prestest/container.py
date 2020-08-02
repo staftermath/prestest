262fa9984fa4a6dee47026f571fcb5e1efbb7560
Weiwen <gwengww@gmail.com>
2020-08-02 14:55:37 -0500

    add container start, stop and is_started method

--- prestest/container.py
+++ prestest/container.py
@@ -1,2 +1,41 @@
 """implement functions to build, start, stop and clean up containers
 """
+import subprocess
+from pathlib import Path, PosixPath
+from typing import Union
+import docker
+
+import logging
+
+CONTAINER_NAMES = {
+    "hive-metastore": "docker-hive_hive-metastore_1",
+    "datanode": "docker-hive_datanode_1",
+    "namenode": "docker-hive_namenode_1",
+    "hive-server": "docker-hive_hive-server_1",
+    "presto_coordinator": "docker-hive_presto-coordinator_1",
+    "hive-metastore-postgresql": "docker-hive_hive-metastore-postgresql_1"
+}
+
+class Container:
+    def __init__(self, docker_folder: Union[PosixPath, str]):
+        self.docker_folder = Path(docker_folder).resolve()
+        self.client = docker.from_env()
+
+    def start(self):
+        command = "docker-compose up -d"
+        process = subprocess.Popen(command, cwd=self.docker_folder, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+
+    def stop(self):
+        command = f"docker-compose stop"
+        process = subprocess.Popen(command, cwd=self.docker_folder, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+
+    def is_started(self):
+        for component, name in CONTAINER_NAMES.items():
+            container = self.client.containers.get(name)
+            if container.status != "running":
+                logging.debug(f"[{component}] {name} is not running")
+                return False
+
+        return True
---
+++ tests/test_container.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import pytest
+import subprocess
+
+from prestest.container import Container
+
+DOCKER_FOLDER = Path(".").resolve().parent / "docker-hive"
+
+@pytest.fixture()
+def container() -> Container:
+    return Container(docker_folder=DOCKER_FOLDER)
+
+
+@pytest.fixture()
+def start_container(container):
+    container.start()
+
+
+def test_start_stop_and_is_started(container):
+    try:
+        process = subprocess.Popen(f"docker-compose stop",
+                                   cwd=DOCKER_FOLDER,
+                                   shell=True,
+                                   stdout=subprocess.PIPE)
+        process.wait()
+    except:
+        pass
+
+    container.start()
+    result = container.is_started()
+    assert result
+
+    container.stop()
+    result = container.is_started()
+    assert not result
